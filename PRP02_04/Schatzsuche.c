5dd9a081f86c275ea305a93194ebdbd975207787
Torben Burandt <torben.burandt@gmx.de>
2023-06-11 15:41:06 +0200

    first

---
+++ PRP02_04/Schatzsuche.c
@@ -0,0 +1,15 @@
+/*
+Dateiname: Schatzsuche.c
+Autor: Torben Burandt & Artem H.
+Datum: 11.06.2023
+Version: 1.1
+Ziel: Programm erzeugt random Zahl die der Benutzer anschließend erraten muss, dabei wird eine Liste zum speichern der eingabe verwendet
+*/
+
+
+int main() {
+
+
+
+	return;
+}
